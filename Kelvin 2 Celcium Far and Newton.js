174fa74418f402c9e4861a48b56a23d45eb21fc2
arthurSlyusar <101943832+arthurSlyusar@users.noreply.github.com>
2022-03-20 12:37:10 +0200

    Add files via upload

---
+++ Kelvin 2 Celcium Far and Newton.js
@@ -0,0 +1,19 @@
+Kelvin 2 Celcium Far and Newton
+
+const kelvin = '590';  
+// just a kelvin degrees - donr
+let celsius = kelvin -273; // create and calculate celsius - correct done
+
+let fahrenheit = celsius * (9/5) + 32; // create new wirh a number type - correct done
+
+fahrenheit = Math.floor(fahrenheit); // округлить градусы 
+
+console.log(`The temperature is ${fahrenheit} degress Fahrenheit`);
+
+
+let newton = celsius * (33/100);
+
+newton = Math.floor(newton);
+
+console.log(`The temperature is ${newton} in Newton`);
+
